a386f63f5c19eec1eb4c8965c73b6dfbd48af5c1
Jason Nguyen <jhnwoo.dev@gmail.com>
2023-06-19 14:51:32 -0700

    working on card movements

--- src/components/CardsHome/index.js
+++ src/components/CardsHome/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './style.css';
 
 export const CardsHome = (props) => {
-	return (
-		<section className={props.cardStyle}>
-			<h1>{props.title}</h1>
-			<button onClick={props.handleActiveState}>expand</button>
-		</section>
-	);
+    return (
+        <section className={props.cardStyle + " all-cards"}>
+            <h1>{props.title}</h1>
+            <button onClick={props.handleActiveState} value={props.title}>expand</button>
+        </section>
+    );
 };
 export default CardsHome;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,38 +3,64 @@ import CardsHome from '../../components/CardsHome';
 import './style.css';
 
 export const Home = () => {
-	const [aboutStyle, setAboutStyle] = useState('default-card');
-	const [communityStyle, setCommunityStyle] = useState('default-card');
-	const [calendarStyle, setCalendarStyle] = useState('default-card');
+    const [aboutStyle, setAboutStyle] = useState('default-card');
+    const [communityStyle, setCommunityStyle] = useState('default-card');
+    const [calendarStyle, setCalendarStyle] = useState('default-card');
 
-	const handleActiveState = (e) => {
-		console.log(e.target);
-		// setAboutStyle('active-card');
-		// setCommunityStyle('nonactive-card');
-		// setCalendarStyle('nonactive-card');
-	};
+    const handleClose = (e) => {
+        setAboutStyle('default-card');
+        setCommunityStyle('default-card');
+        setCalendarStyle('default-card');
 
-	return (
-		<section class="page">
-			<section id="home-card-container">
-				<CardsHome
-					title="about"
-					cardStyle={aboutStyle}
-					handleActiveState={handleActiveState}
-				/>
-				<CardsHome
-					title="community"
-					cardStyle={communityStyle}
-					handleActiveState={handleActiveState}
-				/>
-				<CardsHome
-					title="calendar"
-					cardStyle={calendarStyle}
-					handleActiveState={handleActiveState}
-				/>
-			</section>
-		</section>
-	);
+        // e.target.innerHTML = "expand"
+    }
+
+    const handleActiveState = (e) => {
+        console.log(e.target.innerHTML);
+
+        if (e.target.innerHTML !== "close") {
+            if (e.target.value === "about") {
+                setAboutStyle('active-card');
+                setCommunityStyle('nonactive-card');
+                setCalendarStyle('nonactive-card');
+            }
+            else if (e.target.value === "community") {
+                setCommunityStyle('active-card');
+                setAboutStyle('nonactive-card');
+                setCalendarStyle('nonactive-card');
+            }
+            else {
+                setCalendarStyle('active-card');
+                setCommunityStyle('nonactive-card');
+                setAboutStyle('nonactive-card');
+            }
+        }
+        e.target.innerHTML = "close"
+        e.target.addEventListener("click", handleClose)
+    };
+
+
+    return (
+        <section className="page">
+            <section id="home-card-container">
+                <CardsHome
+                    title="about"
+                    cardStyle={aboutStyle}
+                    handleActiveState={handleActiveState}
+                />
+                <CardsHome
+                    title="community"
+                    cardStyle={communityStyle}
+                    handleActiveState={handleActiveState}
+                />
+                <CardsHome
+                    title="calendar"
+                    cardStyle={calendarStyle}
+                    handleActiveState={handleActiveState}
+                />
+            </section>
+        </section>
+    );
 };
 
 export default Home;
