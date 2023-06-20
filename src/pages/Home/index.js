9fa9ea835d01320d3f6810496bd9e7ae4b0f10d9
agtTwilight <lukasmacmillen@gmail.com>
2023-06-20 09:10:36 -0700

    added stateful change to buttons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,66 +1,65 @@
+import { click } from '@testing-library/user-event/dist/click';
 import React, { useEffect, useState } from 'react';
 import CardsHome from '../../components/CardsHome';
 import './style.css';
 
 export const Home = () => {
-    const [aboutStyle, setAboutStyle] = useState('default-card');
-    const [communityStyle, setCommunityStyle] = useState('default-card');
-    const [calendarStyle, setCalendarStyle] = useState('default-card');
+	const [aboutStyle, setAboutStyle] = useState('default-card');
+	const [communityStyle, setCommunityStyle] = useState('default-card');
+	const [calendarStyle, setCalendarStyle] = useState('default-card');
+	const [clickType, setClickType] = useState('expand');
 
-    const handleClose = (e) => {
-        setAboutStyle('default-card');
-        setCommunityStyle('default-card');
-        setCalendarStyle('default-card');
+	const handleClose = (e) => {
+		setAboutStyle('default-card');
+		setCommunityStyle('default-card');
+		setCalendarStyle('default-card');
+		setClickType('expand');
+	};
 
-        // e.target.innerHTML = "expand"
-    }
+	const handleSpotlight = (e) => {
+		if (e.target.value === 'about') {
+			setAboutStyle('active-card');
+			setCommunityStyle('nonactive-card');
+			setCalendarStyle('nonactive-card');
+		} else if (e.target.value === 'community') {
+			setCommunityStyle('active-card');
+			setAboutStyle('nonactive-card');
+			setCalendarStyle('nonactive-card');
+		} else {
+			setCalendarStyle('active-card');
+			setCommunityStyle('nonactive-card');
+			setAboutStyle('nonactive-card');
+		}
+		setClickType('close');
+	};
 
-    const handleActiveState = (e) => {
-        console.log(e.target.innerHTML);
-
-        if (e.target.innerHTML !== "close") {
-            if (e.target.value === "about") {
-                setAboutStyle('active-card');
-                setCommunityStyle('nonactive-card');
-                setCalendarStyle('nonactive-card');
-            }
-            else if (e.target.value === "community") {
-                setCommunityStyle('active-card');
-                setAboutStyle('nonactive-card');
-                setCalendarStyle('nonactive-card');
-            }
-            else {
-                setCalendarStyle('active-card');
-                setCommunityStyle('nonactive-card');
-                setAboutStyle('nonactive-card');
-            }
-        }
-        e.target.innerHTML = "close"
-        e.target.addEventListener("click", handleClose)
-    };
-
-
-    return (
-        <section className="page">
-            <section id="home-card-container">
-                <CardsHome
-                    title="about"
-                    cardStyle={aboutStyle}
-                    handleActiveState={handleActiveState}
-                />
-                <CardsHome
-                    title="community"
-                    cardStyle={communityStyle}
-                    handleActiveState={handleActiveState}
-                />
-                <CardsHome
-                    title="calendar"
-                    cardStyle={calendarStyle}
-                    handleActiveState={handleActiveState}
-                />
-            </section>
-        </section>
-    );
+	return (
+		<section className="page">
+			<section id="home-card-container">
+				<CardsHome
+					title="about"
+					cardStyle={aboutStyle}
+					clickType={clickType}
+					handleSpotlight={handleSpotlight}
+					handleClose={handleClose}
+				/>
+				<CardsHome
+					title="community"
+					cardStyle={communityStyle}
+					clickType={clickType}
+					handleSpotlight={handleSpotlight}
+					handleClose={handleClose}
+				/>
+				<CardsHome
+					title="calendar"
+					cardStyle={calendarStyle}
+					clickType={clickType}
+					handleSpotlight={handleSpotlight}
+					handleClose={handleClose}
+				/>
+			</section>
+		</section>
+	);
 };
 
 export default Home;
