b96a59c45b80c99d2c0e83ba04c4dde4f11038b8
Jason Nguyen <jhnwoo.dev@gmail.com>
2023-06-21 15:20:27 -0700

    updates to store button and dropdown

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,20 +4,22 @@ import "./style.css";
 export const Navigation = () => {
     return (
         <nav className="navbar">
-            <a href="#about">About</a>
-            <a href="#membership">Memberships</a>
+            <a href="#about" className="a-padding">About</a>
+            <a href="#membership" className="a-padding">Memberships</a>
             <div className="dropdown">
-                <button className="dropbtn">Shop
-                    <i className="fa fa-caret-down"></i>
-                </button>
+                <a href="shopall">
+                    <button className="dropbtn ">Shop
+                        {/* <i className="fa fa-caret-down"></i> */}
+                    </button>
+                </a>
                 <div className="dropdown-content">
-                    <a href="#sbd">SBD</a>
+                    <a href="#sbd" >SBD</a>
                     <a href="#frictionlabs">FrictionLabs</a>
                     <a href="#110p">110Percent</a>
                 </div>
             </div>
-            <a href="#events">Events</a>
-            <a href="#faq">FAQ</a>
+            <a href="#events" className="a-padding">Events</a>
+            <a href="#faq" className="a-padding">FAQ</a>
         </nav>
     );
 };
