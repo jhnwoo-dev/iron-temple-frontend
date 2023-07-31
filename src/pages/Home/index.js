2f94226e2fb338b9115dcfd55fa369a1c2215fbd
agtTwilight <lukasmacmillen@gmail.com>
2023-07-31 11:33:30 -0700

    some more basic main section additions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from '../../assets/images/Iron-temple-PNG-with-border.png';
+import logo from '../../assets/images/logo.png';
 import './style.css';
 
 export const Home = () => {
 	return (
 		<section id="home">
-			<img id="home-logo" src={logo} alt="iron temple logo"></img>
+			<div id="home-left">
+				<h1 id="new-standard">The New Standard</h1>
+				<button className="glow-btn" id="join-btn">
+					JOIN NOW
+				</button>
+			</div>
+			<div id="home-right">
+				<img id="home-logo" src={logo}></img>
+			</div>
 		</section>
 	);
 };
